3be7dd551b46d9643c5cd83f6dfd14a6f7cc3503
Kevin Nicholas Almario <kevinnicholas.almario@infor.com>
2022-06-09 01:21:48 +0800

    Added routes and controller methods

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const express = require("express")
 const mongoose = require('mongoose')
-const { MONGO_USER, MONGO_PASSWORD, MONGO_IP, MONGO_PORT } = require("./config/config")
 
 const app = express()
+const { MONGO_USER, MONGO_PASSWORD, MONGO_IP, MONGO_PORT } = require("./config/config")
 const mongoURL = `mongodb://${MONGO_USER}:${MONGO_PASSWORD}@${MONGO_IP}:${MONGO_PORT}/?authSource=admin`
 
 const connectWithRetry = () =>
@@ -22,11 +22,15 @@ const connectWithRetry = () =>
 
 connectWithRetry()
 
+app.use(express.json())
+
 app.get("/", (req, res) => 
 {
     res.send("<h2>This is Kevin</h2>")
 })
 
-const port = process.env.PORT || 3000
+const postRouter = require("./routes/postRoute")
+app.use("/api/v1/posts", postRouter)
 
+const port = process.env.PORT || 3000
 app.listen(port, () => console.log(`listening on port ${port}`))
